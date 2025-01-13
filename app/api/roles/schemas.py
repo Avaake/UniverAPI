2dc0eaf3d2744057465e08491662f8198d62d996
_Avvake_ <denisergo18@gmail.com>
2025-01-13 23:38:26 +0200

    update HTTP_500_INTERNAL_SERVER_ERROR

--- app/api/roles/schemas.py
+++ app/api/roles/schemas.py
@@ -3,7 +3,7 @@ from typing import Annotated, Optional
 
 
 class BaseRoleSchema(BaseModel):
-    name: Annotated[str, Field(description="role's name")]
+    name: Annotated[str, Field(min_length=4, max_length=30, description="role's name")]
     model_config = ConfigDict(from_attributes=True)
 
 
